37f4cea08c2a5ff5254a841d38a134fbbc1d2d35
saiklonee <saiprithvitech@gmail.com>
2026-02-05 23:54:12 +0530

    add home file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import MainBanner from "../components/MainBanner";
+import Categories from "../components/Categories";
+import BestSeller from "../components/BestSeller";
+import NewsLetter from "../components/NewsLetter";
+import SecondaryBanner from "../components/SecondaryBanner";
+import NewArrivals from "../components/NewArrivals";
+import FeaturedBrands from "../components/FeaturedBrands";
 
 const Home = () => {
     return (
-        <div>Home</div>
-    )
-}
+        <div className="mt-5">
+            <MainBanner />
+            <Categories />
+            <BestSeller />
+            <FeaturedBrands />
+            <NewArrivals />
+            <SecondaryBanner />
+            <NewsLetter />
+        </div>
+    );
+};
 
-export default Home
+export default Home;
