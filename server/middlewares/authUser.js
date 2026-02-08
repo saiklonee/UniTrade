5402e40938c890837806f3ee972b27e36a499a18
saiklonee <saiprithvitech@gmail.com>
2026-02-08 14:35:44 +0530

    complete db cloudinary multer setup

---
+++ server/middlewares/authUser.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const authUser = async (req, res, next) => {
+    const { token } = req.cookies;
+    if (!token) {
+        return res.json({ success: false, message: "Not Authorized" });
+    }
+
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        if (tokenDecode.id) {
+            req.body = req.body || {};
+            req.body.userId = tokenDecode.id;
+        } else {
+            return res.json({ success: false, message: "Not Authorized" });
+        }
+
+        next();
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+export default authUser;
--- server/server.js
+++ server/server.js
@@ -2,19 +2,24 @@ import express from "express"
 import "dotenv/config"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./routes/userRoute.js"
+import connectDB from "./configs/db.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+await connectDB();
+await connectCloudinary();
 
 // Allow multiple origins
-
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
 ];
 
+//Middle Ware Configuration
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -27,6 +32,9 @@ app.use(
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+// apis
+app.use("/api/v1/user", userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
